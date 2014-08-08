a30d6c17496f3aa1e2cd5ebb87338eb26b3a3786
Espen Hovlandsdal <espen@hovlandsdal.com>
2014-08-08 15:40:02 +0200

    Added a config loadable by Imbo

---
+++ config/config.php
@@ -0,0 +1,8 @@
+<?php
+return array(
+    'eventListeners' => array(
+        // Image transformations
+        'earlybird' => 'ImboHipsta\Earlybird',
+        'inkwell' => 'ImboHipsta\Inkwell',
+    ),
+);
